45e27955a55fcd6ae6f460c7d5a73ed96fb3b064
sushthecoda <coolsushaan@gmail.com>
2022-04-27 02:41:02 +0530

    first

---
+++ nftmarketplace/config.js
@@ -0,0 +1,11 @@
+export const nftaddress = "0xcFaBab7f632Aa937A10247C10Ea056084447B695";
+export const nftmarketaddress = "0x97162270156c952b6aE5923a034a34Dc621cF646";
+//EDIT !!!
+let x = 1;
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
---
+++ nftmarketplace/hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "9248e81ccb9d4abcb10ee86d56d23b73"
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account  of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat:{
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+  },
+  solidity: "0.8.4",
+};
